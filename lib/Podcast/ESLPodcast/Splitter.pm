d394fdd499f9c891cec4ce21cd61786608c479a2
Yohei Yamaguchi <joker13meister@gmail.com>
2013-02-24 22:21:12 +0900

    completed until split a mp3

--- lib/Podcast/ESLPodcast/Splitter.pm
+++ lib/Podcast/ESLPodcast/Splitter.pm
@@ -3,15 +3,153 @@ package Podcast::ESLPodcast::Splitter;
 use warnings;
 use strict;
 use Carp;
+use LWP::UserAgent;
+use File::Basename;
+use XML::FeedPP;
+use MP3::Splitter;
+use Data::Dumper;
+use Time::Piece;
+use List::MoreUtils qw(any all);
 
-use version; $VERSION = qv('0.0.3');
+use version;
+my $VERSION = qv('0.0.3');
 
-# Other recommended modules (uncomment to use):
-#  use IO::Prompt;
-#  use Perl6::Export;
-#  use Perl6::Slurp;
-#  use Perl6::Say;
+use constant FEEDURI => 'http://feeds.feedburner.com/EnglishAsASecondLanguagePodcast?format=xml';
 
+# constructor
+sub new {
+    my ($class, @args) = @_;
+    my %args = ref $args[0] eq 'HASH' ? %{$args[0]} : @args;
+    my $self = { %args };
+    $self->{n} ||= 5;
+    bless $self, $class;
+}
+
+# fetch a feed information for the ESLPodcast
+sub fetch_feed {
+    my $feed = XML::FeedPP->new(FEEDURI);
+    printf STDERR "===============FEED INFO================\n";
+    printf STDERR "Title: %s\n", $feed->title;
+    printf STDERR "Date: %s\n", $feed->pubDate;
+    printf STDERR "Copyright: %s\n", $feed->copyright;
+    printf STDERR "Link: %s\n", $feed->link;
+    printf STDERR "Language: %s\n", $feed->language;
+    printf STDERR "========================================\n";
+    return $feed;
+}
+
+sub run {
+    my $self = shift;
+    my $feed = fetch_feed();
+    my $num_mp3s = 0;
+    for my $item ($feed->get_item()) {
+        if ($item->title =~ /English Cafe/) {
+            printf STDERR "Skip: %s\n", $item->title;
+            next;
+        }
+        last if ($num_mp3s > $self->{n});
+
+        printf STDERR "\tURL: %s\n\tTitle: %s\n\tguid: %s\n\tDescription: %s\n", 
+            $item->link, $item->title, $item->guid, ""; # $item->description;
+
+        my $durations = extract_durations($item->description);
+        next if (!defined($durations));
+
+        my $uri = $item->guid();
+        my $mp3_file = basename($uri);
+        if (!download_mp3(\$uri, \$mp3_file)) {
+            printf STDERR "download failure: %s\n", $mp3_file;
+            next;
+        }
+        if (split_mp3(\$mp3_file, $durations)) {
+            printf STDERR "split failure: %s\n", $mp3_file;
+        }
+
+        ++$num_mp3s;
+    }
+    printf STDERR "DONE!";
+}
+
+# extract durations for each dialog
+sub extract_durations {
+    my ($desc) = @_;
+
+    # find started at
+    my $started_at = {slow => undef, explanation => undef, fast => undef};
+    for (split(/[\n|\r\n]/, $desc)) {
+        chomp;
+        if (/Slow (dialog|dialogue):\s*(\d+:\d+)/) {
+            $started_at->{slow} = $2;
+        }
+        if (/Explanations:\s*(\d+:\d+)/) {
+            $started_at->{explanation} = $1;
+        }
+        if (/Fast (dialog|dialogue):\s*(\d+:\d+)/) {
+            $started_at->{fast} = $2;
+        }
+    }
+    # print Dumper($started_at);
+    if (!(all {defined($_)} values %$started_at)) {
+        printf STDERR "something wrong in this feed\n";
+        return undef;
+    }
+
+    # find durations
+    my $durations = {slow=>undef, explanation=>undef, fast=>undef};
+    $durations->{slow} = [
+        $started_at->{slow},
+        find_duration($started_at->{explanation},$started_at->{slow})
+    ];
+    $durations->{explanation} = [
+        $started_at->{explanation},
+        find_duration($started_at->{fast},$started_at->{explanation})
+    ];
+    $durations->{fast} = [
+        $started_at->{fast},
+        "=INF"
+    ];
+    return $durations;
+}
+
+# find a duration
+sub find_duration {
+    my ($from, $to) = @_;
+    my $t1 = Time::Piece->strptime($from, "%M:%S");
+    my $t2 = Time::Piece->strptime($to,   "%M:%S");
+    my $diff = $t2 - $t1;
+    return $diff->seconds;
+}
+
+# split a mp3 file into three files
+sub split_mp3 {
+    my ($mp3_file, $durations) = @_;
+    mp3split($$mp3_file, {verbose => 1}, 
+        $durations->{start}, $durations->{explanation},$durations->{fast});
+    return 1;
+}
+
+# download a mp3 file
+sub download_mp3 {
+    my ($uri, $mp3_file) = @_;
+    open(my $fh, '>', $$mp3_file) or die "$$mp3_file: $!";
+    my $res = LWP::UserAgent->new->get(
+        $$uri,
+        ':content_cb' => sub {
+            my ($chunk, $res, $proto) = @_;
+            print $fh $chunk;
+            my $size = tell $fh;
+            if (my $total = $res->header('Content-Length')) {
+                printf "%d/%d (%f%%)\r", $size, $total, $size/$total*100;
+            } else {
+                printf "%d/Unknown bytes\r", $size;
+            }
+        }
+    );
+    close $fh;
+    print "\n", $res->status_line, "\n";
+    unlink $$mp3_file if (!$res->is_success);
+    return $res->is_success;
+}
 
 # Module implementation here
 
@@ -21,7 +159,7 @@ __END__
 
 =head1 NAME
 
-Podcast::ESLPodcast::Splitter - [One line description of module's purpose here]
+Podcast::ESLPodcast::Splitter - Split ESLPodcast into fast, slow and explanation dialogs.
 
 
 =head1 VERSION
