31586a731787c1fe5ed700e221e66dd724305d22
Yohei Yamaguchi <joker13meister@gmail.com>
2013-02-24 23:42:36 +0900

    updated perldoc

--- lib/Podcast/ESLPodcast/Splitter.pm
+++ lib/Podcast/ESLPodcast/Splitter.pm
@@ -45,7 +45,7 @@ sub run {
     my $num_mp3s = 1;
     for my $item ( $feed->get_item() ) {
         if ( $item->title =~ /English Cafe/ ) {
-            printf STDERR "Skip: %s\n", $item->title;
+            printf STDERR "skip: %s\n", $item->title;
             next;
         }
         last if ( $num_mp3s > $self->{n} );
@@ -171,34 +171,35 @@ This document describes Podcast::ESLPodcast::Splitter version 0.0.1
 
     use Podcast::ESLPodcast::Splitter;
 
-    Podcast::ESLPodcast::Splitter->new()->run();
+    $splitter = Podcast::ESLPodcast::Splitter->new;
+    $splitter->run();
 
 =head1 DESCRIPTION
 
-    
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
+    The "Podcast::ESLPodcast::Splitter" is a class to split a ESLPodcast mp3 file
+    into 3 dialogs, fast, slow and explanation dialogs.
+    You can listen to these dialogs as you like.
 
+    This module works as follows.
+    1. fetch a XML feed for the ESLPodcast.
+    2. parse XML and extract duration information and mp3 file URIs.
+    3. split mp3 files according to the duration information.
 
 =head1 INTERFACE 
 
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
-
+    $splitter->run
+        do mentioned above
 
 =head1 DIAGNOSTICS
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
+    "skip: English Cafe"
+        skip because an English Cafe mp3 files cannot be splitted.
 
-=over
+    "download failure"
+        cannot dowload a mp3 file.
+
+    "split failure"
+        cannot split a downloaded mp3 file.
 
 =item C<< Error message here, perhaps with %s placeholders >>
 
@@ -215,49 +216,21 @@ This document describes Podcast::ESLPodcast::Splitter version 0.0.1
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
 Podcast::ESLPodcast::Splitter requires no configuration files or environment variables.
 
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
 
 None.
 
 
 =head1 INCOMPATIBILITIES
 
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
 None reported.
 
 
 =head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
 
 No bugs have been reported.
 
